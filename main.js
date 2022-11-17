24f53b86b6f7c48b99523ff26af1ebd1d85ecff2
Zakharosomaha <khakim.zakhriddin@mail.com>
2022-11-17 17:30:26 +0600

    dz1

---
+++ main.js
@@ -0,0 +1,35 @@
+//DZ
+// let arr = 0;
+// function recursive() {
+//     arr++;
+//     console.log(arr, 'array');
+//     if (arr === 3) {
+//         return
+//     }else {
+//         return recursive()
+//     }
+// }
+// recursive()
+
+////////////////////////2 task///////////////////////////////
+
+// let array = [4,5,6]
+// let newarray = array.unshift(1,2,3)
+// let zaebarray = array.push(7,8,9,10)
+// console.log(array);
+
+
+///////////////////////3 task//////////////////////////////////
+
+// let arraybay = [1,2,3,4,5];
+// console.log(arraybay[arraybay.length -1]);
+
+///////////////////////4 task ////////////////
+
+// function countChar(str, subStr) {
+//     let count = 'loremipsumdolor';
+//     console.log(count.match(/o/g));
+// }
+// countChar()
+
+/////////////////////////////////////////////////////////////////
